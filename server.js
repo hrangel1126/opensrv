9a199a1fc9958d6e4fbd718e1ac2ac927a33b67c
Hebert Rangel AKA Hebertuz <hebert.rogelio@gmail.com>
2024-11-28 13:46:49 -0600

    test server.js

--- server.js
+++ server.js
@@ -2,83 +2,30 @@ const express = require('express');
 const multer = require('multer');
 const pdfParse = require('pdf-parse');
 const { Configuration, OpenAIApi } = require('openai');
-import { OpenAI } from "openai";
-const openai = new OpenAI();
 const cors = require('cors');
 require('dotenv').config();
-
+const OpenAI = require("openai");
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+});
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const upload = multer({ dest: 'uploads/' });
-
-// const openai = new OpenAIApi({
-//     apiKey: process.env.OPENAI_API_KEY
-//     });
-openai.apiKey({
-    apiKey: process.env.OPENAI_API_KEY
-});
-const tools = [
-    {
-        type: "function",
-        function: {
-            name: "get_weather",
-            parameters: {
-                type: "object",
-                properties: {
-                    location: { type: "string" },
-                    unit: { type: "string", enum: ["c", "f"] },
-                },
-                required: ["location", "unit"],
-                additionalProperties: false,
-            },
-        },
-    }
-];
-
-app.post('/weather', async (req, res) =>{
+app.get('/weather', async (req, res) =>{
     try{
         const response = await openai.chat.completions.create({
             model: "gpt-4o",
-            messages: [{"role": "user", "content": "What's the weather like in Paris today?"}],
-            tools,
+            messages: [{"role": "user", "content": "WHat is a fibonacci funciton"}],
         })
-        console.log(response.choices[0].message.tool_calls);
-        res.json({ text: response.choices[0].message.tool_calls });
+        console.log(response.choices[0].message.content);
+        res.json({ text: response.choices[0].message.content});
       
   } catch (error) {
     console.error(error);
     res.status(500).send('Failed to process PDF');
   }
 
-});
-app.post('/upload', upload.single('pdf'), async (req, res) => {
-  try {
-    const pdfBuffer = req.file.stream
-    const pdfData = await pdfParse(pdfBuffer);
-    const vectorStore = await openai.beta.vectorStores.create({ name: req.file.filename,
-        expires_after: { anchor: 'last_active_at', days: 1 } // Expires 1 days after last use
-
-     });
-     const files = req.file.stream;
-    console.log('vectorStoreId', vectorStore.id);
-    await openai.beta.vectorStores.fileBatches.uploadAndPoll(vectorStore.id, { files });
-    const assistant = await openai.beta.assistants.create({
-        name: 'contract Master',
-        model: 'gpt-4',
-        tools: [{ type: 'file_search' }], // retrieval -> file_search
-        tool_resources: { // Instead of file_id, add the Vector Store's ID
-          file_search: {
-            vector_store_ids: [vectorStore.id] // ID of the created Vector Store
-          }
-        }
-      });
-    res.json({ text: pdfData.text });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Failed to process PDF');
-  }
 });
 
 app.post('/ask', async (req, res) => {
--- test.js
+++ test.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+    // const destino = path.join(process.cwd(), 'tmp');
     const destino = path.join(process.cwd(), 'tmp');
     // const upload = multer({ dest: 'uploads/' });
     const upload = multer({ dest: destino });
