8c892a43e488acf817af63a461b3af256193f6c9
Hebert Rangel AKA Hebertuz <hebert.rogelio@gmail.com>
2024-11-28 18:52:40 -0600

    Server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+// imports
+require('dotenv').config();
 const express = require('express');
 const multer = require('multer');
+const path = require('path');
 const pdfParse = require('pdf-parse');
-const { Configuration, OpenAIApi } = require('openai');
-const cors = require('cors');
-require('dotenv').config();
 const OpenAI = require("openai");
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
@@ -12,47 +14,85 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let tempraryImageDirectory;
-if (process.env.DEV && process.env.DEV === 'Yes') {
-  tempraryImageDirectory = path.join(__dirname, `../../tmp/`);
-} else {
-  tempraryImageDirectory = '/tmp/';
-}
+let destino = '';
+// Verify if is a dev site or production to add proper path for temp upload
+   if (process.env.DEV && process.env.DEV === 'Yes') {
+        destino = path.join(__dirname, `../../tmp/`);
+      } else {
+        destino = '/tmp/';
+      }
+    // This is the uplode site in a VPS, but as is versel I need temp path folder HR.
+    const upload = multer({ dest: destino });
+let pdfContent = '';
 
-app.get('/weather', async (req, res) =>{
-    try{
-        const response = await openai.chat.completions.create({
-            model: "gpt-4o",
-            messages: [{"role": "user", "content": "WHat is a fibonacci funciton"}],
-        })
-        console.log(response.choices[0].message.content);
-        res.json({ text: response.choices[0].message.content});
+// Ruta para subir y procesar un archivo PDF
+app.post('/upload', upload.single('pdf'), async (req, res) => {
+    try {
+      const file = req.file;
+  
+      if (!file) {
+        return res.status(400).json({ error: 'Por favor, sube un archivo PDF.' });
+      }
+  
+      // Leer y procesar el archivo PDF
+      const data = await pdfParse(file.path);
+      pdfContent = data.text;
       
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Failed to process PDF');
-  }
+  
+      return res.status(200).json({ message: 'PDF procesado correctamente.', content: pdfContent });
+    } catch (error) {
+      console.error('Error al procesar el PDF:', error);
+      return res.status(500).json({ error: 'Error al procesar el PDF.' });
+    }
+  });
 
-});
+//   Generar el summary
+app.post('/summary', async (req, res) => {
+ 
+    if (!pdfContent) {
+      return res.status(400).json({ error: 'No se detecta el PDF en el sistema.' });
+    }
+   
+    try {
+        // Realiza la consulta a OpenAI
+        const response = await openai.chat.completions.create({
+          model: 'gpt-4',
+          messages: [
+            { role: 'system', content: 'Eres un asistente legal responde preguntas basadas en contenido proporcionado.' },
+            { role: 'user', content: `Contenido proporcionado:\n\n${pdfContent}\n\nPreguntas: quienes son los interesados en el contrato,\nFechas importantes en el contrato\npenalidades identificadas\ncostos ` },
+          ],
+        });
+        // response.choices[0].message;
+        const answer = response
+        console.log(answer)
+        return res.status(200).json({ answer });
+      } catch (error) {
+        console.error('Error al consultar OpenAI:', error);
+        return res.status(500).json({ error: 'Error al consultar OpenAI.' });
+      }
+      
+   
+  });
 
-app.post('/ask', async (req, res) => {
-  try {
-    const { text, question } = req.body;
-
-    const response = await openai.createChatCompletion({
-      model: 'gpt-4',
-      messages: [
-        { role: 'system', content: 'You are an assistant knowledgeable about PDFs.' },
-        { role: 'user', content: `Here's the content of a PDF: "${text}". Now, answer this question: "${question}".` },
-      ],
-    });
-
-    res.json({ answer: response.data.choices[0].message.content });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Failed to process question');
-  }
-});
-
+  app.post('/ask', async (req, res) => {
+    const { question } = req.body;
+    try {
+        // Realiza la consulta a OpenAI
+        const response = await openai.chat.completions.create({
+          model: 'gpt-4',
+          messages: [
+            { role: 'system', content: 'Eres un asistente legal responde preguntas basadas en contenido proporcionado.' },
+            { role: 'user', content: `El contenido proporcionado este:\n\n${pdfContent}\n\nPreguntas: ${question}` },
+          ],
+        });
+        // response.choices[0].message;
+        const answer = response
+        console.log(answer)
+        return res.status(200).json({ answer });
+      } catch (error) {
+        console.error('Error al consultar OpenAI:', error);
+        return res.status(500).json({ error: 'Error al consultar OpenAI.' });
+      }
+  });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- test.js
+++ test.js
@@ -13,22 +13,20 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-    // const destino = path.join(process.cwd(), 'tmp');
     // const destino = path.join(process.cwd(), 'tmp');
     let destino = '';
+    // Verify if is a dev site or production to add proper path for temp upload
     if (process.env.DEV && process.env.DEV === 'Yes') {
         destino = path.join(__dirname, `../../tmp/`);
       } else {
         destino = '/tmp/';
       }
-    // const upload = multer({ dest: 'uploads/' });
+    // This is the uplode site in a VPS, but as is versel I need temp path folder
+      // const upload = multer({ dest: 'uploads/' });
     const upload = multer({ dest: destino });
-
-    // const elpedf = multer({ storage });
-
 let pdfContent = '';
 
+// simple function to test the api in versel
 app.get('/wea', async (req, res) =>{
     try{
         const completion = await openai.chat.completions.create({
@@ -46,10 +44,12 @@ app.get('/wea', async (req, res) =>{
       
   } catch (error) {
     console.error(error);
-    res.status(500).send('Failed to process PDF');
+    res.status(500).send('Failed to process opeai');
   }
 
 });
+
+// function to upload and read PDF
   app.post('/pdf', upload.single('pdf'), async (req, res) => {
     console.log('funning');
     try {
@@ -60,6 +60,7 @@ app.get('/wea', async (req, res) =>{
       }
   
       // Leer y procesar el archivo PDF
+    //   i parce the PDF as is easier than create a Vector for Openai,m openai upload as vector is in beta version
       const data = await pdfParse(file.path);
       pdfContent = data.text;
     } catch (error) {
@@ -72,8 +73,8 @@ app.get('/wea', async (req, res) =>{
         const response = await openai.chat.completions.create({
           model: 'gpt-4',
           messages: [
-            { role: 'system', content: 'Eres un asistente legal responde preguntas basadas en un PDF cargado.' },
-            { role: 'user', content: `El contenido del PDF es este:\n\n${pdfContent}\n\nPreguntas: quienes son los interesados en el contrato,\nFechas importantes en el contrato\npenalidades identificadas\ncostos ` },
+            { role: 'system', content: 'Eres un asistente legal responde preguntas basadas en contenido proporcionado.' },
+            { role: 'user', content: `El contenido proporcionado este:\n\n${pdfContent}\n\nPreguntas: quienes son los interesados en el contrato,\nFechas importantes en el contrato\npenalidades identificadas\ncostos ` },
           ],
         });
         // response.choices[0].message;
@@ -87,6 +88,23 @@ app.get('/wea', async (req, res) =>{
       
    
   });
+  app.post('/ask', async (req, res) => {
+    try {
+      const { text, question } = req.body;
   
+      const response = await openai.createChatCompletion({
+        model: 'gpt-4',
+        messages: [
+            { role: 'system', content: 'Eres un asistente legal responde preguntas basadas en un PDF cargado.' },
+          { role: 'user', content: `Here's the content of a PDF: "${text}". Now, answer this question: "${question}".` },
+        ],
+      });
+  
+      res.json({ answer: response.data.choices[0].message.content });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Failed to process question');
+    }
+  });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
